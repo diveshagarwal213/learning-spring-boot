3dfc8a0ed81ada354ed32fe46fbeb864a52595ce
Divesh Aggarwal <diveshagarwal213@gmail.com>
2024-12-07 23:44:15 +0530

    first commit

---
+++ src/main/java/com/LearningSpringBoot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.LearningSpringBoot.demo.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello world";
+    }
+    
+}
