05bf14b4296ecfbbb4259f331f1186512cc6df09
Divesh Aggarwal <diveshagarwal213@gmail.com>
2024-12-08 14:29:22 +0530

    added hot reload

--- src/main/java/com/LearningSpringBoot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/LearningSpringBoot/demo/mycoolapp/rest/FunRestController.java
@@ -3,12 +3,16 @@ package com.LearningSpringBoot.demo.mycoolapp.rest;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 public class FunRestController {
     @GetMapping("/")
     public String sayHello() {
         return "Hello world";
     }
-    
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "DailyWorkout";
+    }
+
 }
